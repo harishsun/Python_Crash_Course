20ac911058dc11335ecdc72bf20859e2fed8d053
Harish Sundaram <47561553+harishsun@users.noreply.github.com>
2019-02-20 18:18:45 +0000

    Add files via upload

---
+++ t_shirt.py
@@ -0,0 +1,9 @@
+def make_tshirt(size, message):
+    print("\nThe size of the ordered tshirt is: " + size)
+    print("The message on the tshirt is: " + size)
+    print("Your tshirt is ready to collect!!")
+
+tshirt_size = input("\nEnter the size of your tshirt in inches: ")
+tshirt_message = input("Enter the message to be printed on your tshirt ")
+
+make_tshirt(tshirt_size, tshirt_message)
