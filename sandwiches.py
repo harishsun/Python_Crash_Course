05106fc25535566b94e724597be7d38c2a81d180
Harish Sundaram <47561553+harishsun@users.noreply.github.com>
2019-02-26 09:37:32 +0000

    Add files via upload

---
+++ sandwiches.py
@@ -0,0 +1,11 @@
+def make_sandwiches(*fillings):
+    print("\n----Making Sandwich with the following fillings---- ")
+    for filling in fillings:
+        print(" - " + filling.title())
+
+
+
+
+make_sandwiches('egg')
+make_sandwiches('tomato, cucumber')
+make_sandwiches('pickles', 'jalapenos', 'olives')
